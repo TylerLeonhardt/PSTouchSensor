3d105387008e712dce193d6027c202696ef61e35
Tyler Leonhardt <tylerl0706@gmail.com>
2018-10-19 11:04:02 +0800

    debug

--- test/PSTouchSensor.Tests.ps1
+++ test/PSTouchSensor.Tests.ps1
@@ -6,6 +6,8 @@ Describe 'PSTouchSensor' {
     Context "Get-PSTouchSensor" {
         # This test will only pass if something is touching the sensor
         It 'Can get the value of the TouchSensor' -Skip:$(!$IsRaspberryPi) {
+            (Get-PSTouchSensor).Result
+            Get-GpioPin -Id 7
             (Get-PSTouchSensor).Result | Should -BeTrue
         }
     }
